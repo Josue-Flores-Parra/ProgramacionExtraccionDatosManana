26761015869cb436f5d6fd3ad407e41b2d697581
Josue-Flores-Parra <83735048+Josue-Flores-Parra@users.noreply.github.com>
2023-11-22 13:20:34 -0800

    Update dashboard_olimpiadas_bootstrap.py

--- dashboard_olimpiadas_bootstrap.py
+++ dashboard_olimpiadas_bootstrap.py
@@ -8,7 +8,8 @@ from dash import Dash, dcc, html, dash_table, callback, Input, Output
 # html -----> Dash Html Components
 
 data = pd.read_csv(cd.PATH_ARCHIVO, index_col=0)
-
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 def tarjeta_filtros():
     control = dbc.Card([
@@ -58,6 +59,5 @@ def dashboard():
 
 
 if __name__ == "__main__":
-    app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
     app.layout = dashboard()
     app.run(debug=True)
